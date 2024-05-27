fc6ffbddc13b5006aaddc268c2616252cfce18d3
Mark Ross <mark@xericode.dev>
2024-05-27 10:55:27 -0700

    Cleanup.

--- include/api/Api.h
+++ include/api/Api.h
@@ -22,12 +22,15 @@ public:
     static bool IsLoggedIn();
 
 private:
+    // auth
     static inline std::string member_id;
     static inline std::string token;
 
+    // portfolio
     static inline double balance;
     //static inline double payout;
 
+    // core
     static boost::json::value MakeRequest(std::string_view endpoint, bool doPostMethod = false);
     static boost::json::value MakeRequest(std::string_view endpoint, const boost::json::value &json, bool doPostMethod = false);
 
