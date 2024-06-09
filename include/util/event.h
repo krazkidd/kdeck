ca62c675d65700d98b2c6e1f7021e7986b27504a
Mark Ross <mark@xericode.dev>
2024-06-09 07:00:38 -0700

    Declare and define new event types.

---
+++ include/util/event.h
@@ -0,0 +1,9 @@
+#ifndef EVENT_H
+#define EVENT_H
+
+#include <wx/wx.h>
+
+wxDECLARE_EVENT(EVT_LOGIN, wxCommandEvent);
+wxDECLARE_EVENT(EVT_LOGOUT, wxCommandEvent);
+
+#endif
---
+++ src/util/event.cpp
@@ -0,0 +1,4 @@
+#include "util/event.h"
+
+wxDEFINE_EVENT(EVT_LOGIN, wxCommandEvent);
+wxDEFINE_EVENT(EVT_LOGOUT, wxCommandEvent);
