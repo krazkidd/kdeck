96fd1fd9c52913d14168c23752b41f4b820d4bd9
Mark Ross <mark@xericode.dev>
2024-06-16 10:35:04 -0700

    Make API URL configurable through CMake. Use dev environment URL for debug builds and live environment URL for release builds.

---
+++ include/config.hpp
@@ -0,0 +1,12 @@
+// NOTE: This file only exists to prevent IDE errors. However, it
+//       may interfere with the build process. If you encounter
+//       any issues, please ensure that CMake's generated
+//       config.hpp file is looked up *first*, i.e.
+//
+//           ${CMAKE_BINARY_DIR}/include
+//
+//       before
+//
+//           ${CMAKE_SOURCE_DIR}/include
+
+#include "config.hpp.in"
