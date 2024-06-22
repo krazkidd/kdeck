719598991521617c7dcebe271b40d3ab16401584
Mark Ross <6730333+krazkidd@users.noreply.github.com>
2024-06-22 12:59:48 -0700

    Implement exchange schedule dialog.
    Doesn't include scheduled maintenance or local timezone conversions.

--- include/ui/ExchangeScheduleDialog.hpp
+++ include/ui/ExchangeScheduleDialog.hpp
@@ -8,7 +8,19 @@ class ExchangeScheduleDialog : public wxDialog
 public:
     ExchangeScheduleDialog(wxWindow* parent, wxWindowID winid = wxID_ANY, const wxString &title = "Exchange Schedule");
 
+    void UpdateStuff();
+
+    virtual bool Show(bool show = true) override;
+
 private:
+    wxStaticText* lblSundaySchedule;
+    wxStaticText* lblMondaySchedule;
+    wxStaticText* lblTuesdaySchedule;
+    wxStaticText* lblWednesdaySchedule;
+    wxStaticText* lblThursdaySchedule;
+    wxStaticText* lblFridaySchedule;
+    wxStaticText* lblSaturdaySchedule;
+
     // init
     void Setup();
 };
--- src/ui/ExchangeScheduleDialog.cpp
+++ src/ui/ExchangeScheduleDialog.cpp
@@ -1,3 +1,4 @@
+#include <wx/string.h>
 #include <wx/wx.h>
 
 #include "api/Api.hpp"
@@ -18,17 +19,98 @@ ExchangeScheduleDialog::ExchangeScheduleDialog(wxWindow* parent, wxWindowID wini
 
 void ExchangeScheduleDialog::Setup()
 {
+    wxStaticText* lblSunday = new wxStaticText(this, wxID_ANY, "Sunday:");
+    lblSundaySchedule = new wxStaticText(this, wxID_ANY, "");
+
+    wxStaticText* lblMonday = new wxStaticText(this, wxID_ANY, "Monday:");
+    lblMondaySchedule = new wxStaticText(this, wxID_ANY, "");
+
+    wxStaticText* lblTuesday = new wxStaticText(this, wxID_ANY, "Tuesday:");
+    lblTuesdaySchedule = new wxStaticText(this, wxID_ANY, "");
+
+    wxStaticText* lblWednesday = new wxStaticText(this, wxID_ANY, "Wednesday:");
+    lblWednesdaySchedule = new wxStaticText(this, wxID_ANY, "");
+
+    wxStaticText* lblThursday = new wxStaticText(this, wxID_ANY, "Thursday:");
+    lblThursdaySchedule = new wxStaticText(this, wxID_ANY, "");
+
+    wxStaticText* lblFriday = new wxStaticText(this, wxID_ANY, "Friday:");
+    lblFridaySchedule = new wxStaticText(this, wxID_ANY, "");
+
+    wxStaticText* lblSaturday = new wxStaticText(this, wxID_ANY, "Saturday:");
+    lblSaturdaySchedule = new wxStaticText(this, wxID_ANY, "");
+
+    ///////////////////////////////////////////////////////////////////////////
+
+    wxGridSizer* gridSizer = new wxGridSizer(7, 2, wxSize{10, 10});
+
+    wxSizerFlags flagsLblLeft = wxSizerFlags().CenterVertical().Left();
+    wxSizerFlags flagsLblRight = wxSizerFlags().CenterVertical().Right();
+
+    gridSizer->Add(lblSunday, flagsLblRight);
+    gridSizer->Add(lblSundaySchedule, flagsLblLeft);
+
+    gridSizer->Add(lblMonday, flagsLblRight);
+    gridSizer->Add(lblMondaySchedule, flagsLblLeft);
+
+    gridSizer->Add(lblTuesday, flagsLblRight);
+    gridSizer->Add(lblTuesdaySchedule, flagsLblLeft);
+
+    gridSizer->Add(lblWednesday, flagsLblRight);
+    gridSizer->Add(lblWednesdaySchedule, flagsLblLeft);
+
+    gridSizer->Add(lblThursday, flagsLblRight);
+    gridSizer->Add(lblThursdaySchedule, flagsLblLeft);
+
+    gridSizer->Add(lblFriday, flagsLblRight);
+    gridSizer->Add(lblFridaySchedule, flagsLblLeft);
+
+    gridSizer->Add(lblSaturday, flagsLblRight);
+    gridSizer->Add(lblSaturdaySchedule, flagsLblLeft);
+
+    ///////////////////////////////////////////////////////////////////////////
+
     wxBoxSizer* boxSizer = new wxBoxSizer(wxVERTICAL);
 
-    wxSizerFlags flagsInput = wxSizerFlags().Border(wxALL, 10).Expand();
-
-    // boxSizer->Add(TODO, flagsInput);
+    boxSizer->Add(gridSizer, wxSizerFlags().Border(wxALL, 10).Expand());
 
     wxSizer* szrButton = CreateButtonSizer(wxOK);
     if (szrButton)
     {
-        boxSizer->Add(szrButton);
+        boxSizer->Add(szrButton, wxSizerFlags().Center());
     }
 
     SetSizerAndFit(boxSizer);
+}
+
+void ExchangeScheduleDialog::UpdateStuff()
+{
+    try
+    {
+        ExchangeScheduleResponse status = Api::GetExchangeSchedule();
+
+        lblSundaySchedule->SetLabel(wxString::Format(L"%s—%s ET", status.schedule.standard_hours.sunday.open_time, status.schedule.standard_hours.sunday.close_time));
+        lblMondaySchedule->SetLabel(wxString::Format(L"%s—%s ET", status.schedule.standard_hours.monday.open_time, status.schedule.standard_hours.monday.close_time));
+        lblTuesdaySchedule->SetLabel(wxString::Format(L"%s—%s ET", status.schedule.standard_hours.tuesday.open_time, status.schedule.standard_hours.tuesday.close_time));
+        lblWednesdaySchedule->SetLabel(wxString::Format(L"%s—%s ET", status.schedule.standard_hours.wednesday.open_time, status.schedule.standard_hours.wednesday.close_time));
+        lblThursdaySchedule->SetLabel(wxString::Format(L"%s—%s ET", status.schedule.standard_hours.thursday.open_time, status.schedule.standard_hours.thursday.close_time));
+        lblFridaySchedule->SetLabel(wxString::Format(L"%s—%s ET", status.schedule.standard_hours.friday.open_time, status.schedule.standard_hours.friday.close_time));
+        lblSaturdaySchedule->SetLabel(wxString::Format(L"%s—%s ET", status.schedule.standard_hours.saturday.open_time, status.schedule.standard_hours.saturday.close_time));
+    }
+    catch (std::exception &e)
+    {
+        std::cerr << e.what() << std::endl;
+
+        wxCommandEvent* evt = new wxCommandEvent(EVT_API_ERROR);
+        evt->SetEventObject(this);
+        evt->SetString("Exchange schedule failed!");
+        QueueEvent(evt);
+    }
+}
+
+bool ExchangeScheduleDialog::Show(bool show)
+{
+    UpdateStuff();
+
+    return wxDialog::Show(show);
 }
