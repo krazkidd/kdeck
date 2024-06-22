7df93cb9bd61f076c7a09912d285f6934328bd93
Mark Ross <6730333+krazkidd@users.noreply.github.com>
2024-06-22 12:56:51 -0700

    Implement exchange status dialog.

--- include/ui/ExchangeStatusDialog.hpp
+++ include/ui/ExchangeStatusDialog.hpp
@@ -8,7 +8,14 @@ class ExchangeStatusDialog : public wxDialog
 public:
     ExchangeStatusDialog(wxWindow* parent, wxWindowID winid = wxID_ANY, const wxString &title = "Exchange Status");
 
+    void UpdateStuff();
+
+    virtual bool Show(bool show = true) override;
+
 private:
+    wxStaticText* lblExchangeStatus;
+    wxStaticText* lblTradingStatus;
+
     // init
     void Setup();
 };
--- src/ui/ExchangeStatusDialog.cpp
+++ src/ui/ExchangeStatusDialog.cpp
@@ -18,17 +18,78 @@ ExchangeStatusDialog::ExchangeStatusDialog(wxWindow* parent, wxWindowID winid, c
 
 void ExchangeStatusDialog::Setup()
 {
+    wxStaticText* lblExchange = new wxStaticText(this, wxID_ANY, "Exchange:");
+    lblExchangeStatus = new wxStaticText(this, wxID_ANY, "");
+
+    wxStaticText* lblTrading = new wxStaticText(this, wxID_ANY, "Trading:");
+    lblTradingStatus = new wxStaticText(this, wxID_ANY, "");
+
+    ///////////////////////////////////////////////////////////////////////////
+
+    wxGridSizer* gridSizer = new wxGridSizer(2, 2, wxSize{10, 10});
+
+    wxSizerFlags flagsLblLeft = wxSizerFlags().CenterVertical().Left();
+    wxSizerFlags flagsLblRight = wxSizerFlags().CenterVertical().Right();
+
+    gridSizer->Add(lblExchange, flagsLblRight);
+    gridSizer->Add(lblExchangeStatus, flagsLblLeft);
+
+    gridSizer->Add(lblTrading, flagsLblRight);
+    gridSizer->Add(lblTradingStatus, flagsLblLeft);
+
+    ///////////////////////////////////////////////////////////////////////////
+
     wxBoxSizer* boxSizer = new wxBoxSizer(wxVERTICAL);
 
-    wxSizerFlags flagsInput = wxSizerFlags().Border(wxALL, 10).Expand();
-
-    // boxSizer->Add(TODO, flagsInput);
+    boxSizer->Add(gridSizer, wxSizerFlags().Border(wxALL, 10).Expand());
 
     wxSizer* szrButton = CreateButtonSizer(wxOK);
     if (szrButton)
     {
-        boxSizer->Add(szrButton);
+        boxSizer->Add(szrButton, wxSizerFlags().Center());
     }
 
     SetSizerAndFit(boxSizer);
+}
+
+void ExchangeStatusDialog::UpdateStuff()
+{
+    try
+    {
+        ExchangeStatusResponse status = Api::GetExchangeStatus();
+
+        if (status.exchange_active)
+        {
+            lblExchangeStatus->SetLabel(L"🟢 Active");
+        }
+        else
+        {
+            lblExchangeStatus->SetLabel(L"🔴 Not active");
+        }
+
+        if (status.trading_active)
+        {
+            lblTradingStatus->SetLabel(L"🟢 Active");
+        }
+        else
+        {
+            lblTradingStatus->SetLabel(L"🔴 Not active");
+        }
+    }
+    catch (std::exception &e)
+    {
+        std::cerr << e.what() << std::endl;
+
+        wxCommandEvent* evt = new wxCommandEvent(EVT_API_ERROR);
+        evt->SetEventObject(this);
+        evt->SetString("Exchange status failed!");
+        QueueEvent(evt);
+    }
+}
+
+bool ExchangeStatusDialog::Show(bool show)
+{
+    UpdateStuff();
+
+    return wxDialog::Show(show);
 }
