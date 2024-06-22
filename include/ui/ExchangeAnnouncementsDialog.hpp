5c15fd2d90f14dea28e48cfc3b6bb544ff0d35c3
Mark Ross <6730333+krazkidd@users.noreply.github.com>
2024-06-22 14:18:59 -0700

    Implement exchange announcements dialog.
    This should be considered incomplete because we don't have an example announcement to test.

--- include/ui/ExchangeAnnouncementsDialog.hpp
+++ include/ui/ExchangeAnnouncementsDialog.hpp
@@ -8,7 +8,13 @@ class ExchangeAnnouncementsDialog : public wxDialog
 public:
     ExchangeAnnouncementsDialog(wxWindow* parent, wxWindowID winid = wxID_ANY, const wxString &title = "Exchange Announcements");
 
+    void UpdateStuff();
+
+    virtual bool Show(bool show = true) override;
+
 private:
+    wxBoxSizer* boxSizer;
+
     // init
     void Setup();
 };
--- src/ui/ExchangeAnnouncementsDialog.cpp
+++ src/ui/ExchangeAnnouncementsDialog.cpp
@@ -18,17 +18,63 @@ ExchangeAnnouncementsDialog::ExchangeAnnouncementsDialog(wxWindow* parent, wxWin
 
 void ExchangeAnnouncementsDialog::Setup()
 {
-    wxBoxSizer* boxSizer = new wxBoxSizer(wxVERTICAL);
+    boxSizer = new wxBoxSizer(wxVERTICAL);
 
-    wxSizerFlags flagsInput = wxSizerFlags().Border(wxALL, 10).Expand();
+    wxBoxSizer* szr = new wxBoxSizer(wxVERTICAL);
 
-    // boxSizer->Add(TODO, flagsInput);
+    szr->Add(boxSizer, wxSizerFlags().Border(wxALL, 10).Expand());
 
     wxSizer* szrButton = CreateButtonSizer(wxOK);
     if (szrButton)
     {
-        boxSizer->Add(szrButton);
+        szr->Add(szrButton, wxSizerFlags().Center());
     }
 
-    SetSizerAndFit(boxSizer);
+    SetSizerAndFit(szr);
+}
+
+void ExchangeAnnouncementsDialog::UpdateStuff()
+{
+    boxSizer->Clear();
+
+    try
+    {
+        ExchangeAnnouncementsResponse announcements = Api::GetExchangeAnnouncements();
+
+        wxSizerFlags flags = wxSizerFlags().Border(wxALL, 10); //.Expand();
+
+        if (announcements.announcements.empty())
+        {
+            boxSizer->Add(new wxStaticText(this, wxID_ANY, "No announcements."), flags);
+        }
+        else
+        {
+            for (auto announcement : announcements.announcements)
+            {
+                wxBoxSizer* szr = new wxBoxSizer(wxHORIZONTAL);
+
+                //TODO need to stringify/localize time
+                //szr->Add(new wxStaticText(this, wxID_ANY, announcement.delivery_time), flags);
+                szr->Add(new wxStaticText(this, wxID_ANY, announcement.status), flags);
+                szr->Add(new wxStaticText(this, wxID_ANY, announcement.type), flags);
+                szr->Add(new wxStaticText(this, wxID_ANY, announcement.message), flags);
+            }
+        }
+    }
+    catch (std::exception &e)
+    {
+        std::cerr << e.what() << std::endl;
+
+        wxCommandEvent* evt = new wxCommandEvent(EVT_API_ERROR);
+        evt->SetEventObject(this);
+        evt->SetString("Exchange announcements failed!");
+        QueueEvent(evt);
+    }
+}
+
+bool ExchangeAnnouncementsDialog::Show(bool show)
+{
+    UpdateStuff();
+
+    return wxDialog::Show(show);
 }
